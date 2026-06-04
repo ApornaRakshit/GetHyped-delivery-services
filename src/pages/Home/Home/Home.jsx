a297d7aa59a942e536ae60f2654eb780fbec13bb
ApornaRakshit <rakshit.aporna@gmail.com>
2026-06-04 12:31:44 +0600

    Swipper the brands

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Works from '../Works/Works';
+import Brands from '../Brands/Brands';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
             <Works></Works>
+            <Brands></Brands>
         </div>
     );
 };
